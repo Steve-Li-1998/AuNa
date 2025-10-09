1b530fbf932b4d78e836a0e5576c9532b2976533
Hao <hao3.li@tu-dortmund.de>
2025-10-09 15:55:35 +0200

    **feat(laptimer): add and clean up laptimer configuration and launch files**
    - Added `laptimer.launch.py` for an improved launch process with namespaces and dynamic parameter support.
    - Introduced `laptimer.yaml` for centralized management of laptimer parameters including finish tapes, topics, and services.
    - Removed legacy configuration files and outdated `sample.launch.py` to declutter and align with new project standards.
    - Enhanced integration with ROS2 for improved modularity, maintainability, and deployment flexibility.

---
+++ packages/src/auna_laptimer/launch/laptimer.launch.py
@@ -0,0 +1,58 @@
+from launch_ros.actions import Node, PushRosNamespace
+from launch import LaunchDescription
+from launch.actions import OpaqueFunction, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_context import LaunchContext
+from launch_ros.substitutions import FindPackageShare
+import os
+
+
+def include_launch_description(context: LaunchContext):
+    """Return launch description"""
+
+    launch_description_content = []
+
+    # Get robot_index and construct namespace
+    robot_index = int(os.environ.get('ROBOT_INDEX', '0'))
+    namespace = f"robot{robot_index}"
+
+    # Get path to config file
+    config_file = PathJoinSubstitution([
+        FindPackageShare('auna_laptimer'),
+        'config',
+        'laptimer.yaml'
+    ])
+
+    laptimer_node = Node(
+        package='auna_laptimer',
+        executable='laptimer',
+        name='laptimer_node',
+        output='screen',
+        parameters=[config_file],
+    )
+
+    push_ns = PushRosNamespace(namespace)
+    launch_description_content.append(push_ns)
+
+    # Add the laptimer node to the launch description content
+    launch_description_content.append(laptimer_node)
+
+    return launch_description_content   
+
+
+def generate_launch_description():
+    """Return launch description"""
+    # Launch Description
+    launch_description = LaunchDescription()
+
+    # Declare config_file argument
+    launch_description.add_action(DeclareLaunchArgument(
+        'config_file',
+        default_value='',
+        description='Path to the configuration file for laptimer parameters.'
+    ))
+
+    launch_description.add_action(OpaqueFunction(
+        function=include_launch_description))
+
+    return launch_description
